d21a833baf7a06e925c3d133eb04e6c49992797f
eddah73 <eddahsjr@gmail.com>
2024-06-25 20:53:43 +0300

    arithmetic expression

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+const num1=31;
+const num2=2;
+
+const multiply= num1*num2;
+multiply;
+
+const random = Math.floor(Math.random()*10)+1;
+// random;
+
+const num3=16;
+const num4=6;
+const mod = num3 % num4;
+// mod;
+const max = Math.max(1,5,10,20);
+max;
+// module.exports ={
+//     num1,num2,multiply,random,num3,num4,mod,max
+// };
+
